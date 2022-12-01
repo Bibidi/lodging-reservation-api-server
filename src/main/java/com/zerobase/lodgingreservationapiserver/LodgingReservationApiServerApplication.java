7a27afedceb6450c84dc174648c2619b825b89b0
Bibidi <byungdeuk.kim@gmail.com>
2022-12-02 00:13:20 +0900

    Project Setting

---
+++ src/main/java/com/zerobase/lodgingreservationapiserver/LodgingReservationApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.lodgingreservationapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgingReservationApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgingReservationApiServerApplication.class, args);
+	}
+
+}
